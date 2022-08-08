eeee12b53aa10421b0a72b0b0bc0476b1230a282
Temo Asanishvili <asanishvilitemo@gmail.com>
2022-08-08 12:24:51 +0400

    done test

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { Component } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage/HomePage";
+import Clothes from "./components/Clothes/Clothes";
+import Nav from "./components/Nav/Nav";
+import Tech from "./components/Tech/Tech";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedCurrency: {
+        label: "USD",
+        symbol: "$",
+        __typename: "Currency",
+      },
+    };
+  }
+
+  changeCurrency = (currency) => {
+    this.setState({ selectedCurrency: currency });
+  };
+
+  render() {
+    return (
+      <Router>
+        <header>
+          <Nav changeCurrency={this.changeCurrency} />
+        </header>
+        <main>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <HomePage selectedCurrency={this.state.selectedCurrency} />
+              }
+            />
+            <Route
+              path="/clothes"
+              element={
+                <Clothes selectedCurrency={this.state.selectedCurrency} />
+              }
+            />
+            <Route
+              path="/tech"
+              element={<Tech selectedCurrency={this.state.selectedCurrency} />}
+            />
+          </Routes>
+        </main>
+      </Router>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { ApolloClient, InMemoryCache, HttpLink } from "apollo-boost"
+import { ApolloProvider } from 'react-apollo';
+
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'http://localhost:4000/'
+  }),
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
